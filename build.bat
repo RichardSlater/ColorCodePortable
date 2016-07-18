310e42dd73b0dc0ed951223afe8b46ea9fe2762a
Richard Slater <github@richard-slater.co.uk>
2016-07-18 17:34:57 +0100

    Upgrade MSBuild to 4.0

--- build.bat
+++ build.bat
@@ -5,10 +5,10 @@ if %COMPUTERNAME%==CODEPLEX-BUILD set ArchivePath=D:\Builds\Zips\ColorCode
 if "%1" == "" goto :WithoutTarget
 
 :WithTarget
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe ColorCode.msbuild /p:Configuration=Debug /logger:Kobush.Build.Logging.XmlLogger,"3rdParty\Kobush.Build.dll";BuildResults.xml /t:%*
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ColorCode.msbuild /p:Configuration=Debug /logger:Kobush.Build.Logging.XmlLogger,"3rdParty\Kobush.Build.dll";BuildResults.xml /t:%*
 goto :End
 
 :WithoutTarget
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe ColorCode.msbuild /p:Configuration=Debug /logger:Kobush.Build.Logging.XmlLogger,"3rdParty\Kobush.Build.dll";BuildResults.xml
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ColorCode.msbuild /p:Configuration=Debug /logger:Kobush.Build.Logging.XmlLogger,"3rdParty\Kobush.Build.dll";BuildResults.xml
 
 :End
