7c494c48bb63dba03b6a8f931e66a9a41f81a9b7
The Sharp Ninja <ninja@thesharp.ninja>
2021-11-28 18:02:26 -0600

    Updates for MarkdownServer

---
+++ ColorCode.Facts/globalusings.cs
@@ -0,0 +1,10 @@
+﻿global using Color = ColorCode.Styling.Color;
+
+global using System.Drawing;
+global using ColorCode.Common;
+global using ColorCode.Parsing;
+global using ColorCode.Stubs;
+global using ColorCode.Styling;
+
+global using Xunit;
+
